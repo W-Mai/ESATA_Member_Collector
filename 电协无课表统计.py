6b191fb09754db8338a010245f350bdc41046b74
W-Mai <1341398182@qq.com>
2019-09-30 23:36:05 +0800

    添加了一些注释

--- 电协无课表统计.py
+++ 电协无课表统计.py
@@ -7,6 +7,7 @@ DATAPATH = './Datas/'
 
 files = next(os.walk(DATAPATH))[2]
 
+# 初始化缓冲区
 TargetData = [[[],[],[],[],[],[],[]],
                [[],[],[],[],[],[],[]],
                [[],[],[],[],[],[],[]],
@@ -14,7 +15,7 @@ TargetData = [[[],[],[],[],[],[],[]],
                [[],[],[],[],[],[],[]],
                [[],[],[],[],[],[],[]],]
 
-
+# 将数据加载到内存
 def LoadData():
     totalFile = len(files)
     fileCounter = 0
@@ -23,18 +24,21 @@ def LoadData():
         wb = openpyxl.load_workbook(DATAPATH + fileName)  
         sheet = wb['Main']
     
-        name = sheet.cell(row=11, column=3).value
-        class_ = sheet.cell(row=12, column=3).value
+        name = sheet.cell(row=11, column=3).value           # 读名字
+        class_ = sheet.cell(row=12, column=3).value          # 读班级
     
         print(f">>> Analysis Process : {fileCounter / totalFile:>7.2%} - {name:5}  {class_}")
     
         for i in range(6):
             for j in range(7):
+                # 取出每一格数据
                 tmpVal = sheet.cell(row=4 + i, column=3 + j).value
+                # 如果是0或者没有填也就是没有课，加入缓冲区
                 if  not(tmpVal != None and tmpVal > 0):
                     TargetData[i][j].append(name)
     return fileCounter, fileName, totalFile
 
+# 将数据从内存中写入到磁盘
 def SaveData():
     TargetFile = openpyxl.load_workbook("./TargetExcel_Template.xlsx")  
     TargetSheet = TargetFile['Main']
@@ -42,9 +46,11 @@ def SaveData():
     for i in range(6):
             for j in range(7):
                 print(f'>>> Saving Process : {(i*7+j)/41:>7.2%}')
+                # 将缓冲区中对应表格中的数据化List为String（换行分割）载入表格
                 TargetSheet.cell(row=4 + i, column=3 + j).value = "\n".join(TargetData[i][j])
     return TargetFile
 
+# 以下内容显而易见 不需要注释了
 print("$$ 开始加载数据 ##########################\n")
 LoadData()
 print("\n$$ 加载数据完毕 ##########################\n")
@@ -53,5 +59,6 @@ print("$$ 开始保存数据 ##########################\n")
 TargetFile = SaveData()
 print("\n$$ 保存数据完毕 #########################\n")
 
+# 储存
 TargetFile.save("./TargetExcel.xlsx")
 print("\n$$ 汇总结果已保存到 './TargetExcel.xlsx'\n")
